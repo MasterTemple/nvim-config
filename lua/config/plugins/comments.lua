7abe89bad8d4f7195e96b8be4956b87b193d01cc
Blake Scampone <blake.scampone@gmail.com>
2026-05-28 22:58:31 -0700

    Small changes

--- lua/config/plugins/comments.lua
+++ lua/config/plugins/comments.lua
@@ -25,38 +25,31 @@ vim.pack.add({
 		src = "https://github.com/folke/todo-comments.nvim",
 		-- version = "31e3c38",
 	},
-	{
-		src = "https://github.com/nvim-lua/plenary.nvim",
-	},
 })
 
 --- @type TodoOptions
 local opts = {
 	keywords = {
-		-- CHECK = { icon = "󱉶 ", color = "@lsp.type.string", alt = { "VERIFY" } },
-		-- DONE = { icon = " ", color = "@lsp.type.string", alt = { "FINISHED" } },
-		-- SAFE = { icon = "🦀", color = "@lsp.type.boolean", alt = { "SAFETY" } },
-		-- JUSTIFY = { icon = " ", color = "@keyword.conditional" },
-		-- FEAT = { icon = "", color = "@string.documentation", alt = { "IDEA" } },
-		-- IDK = { icon = "?", color = "@module.builtin", alt = { "HELP", "QUESTION" } },
-
 		CHECK = { icon = "󱉶 ", color = "check", alt = { "VERIFY" } },
 		DONE = { icon = " ", color = "done", alt = { "FINISHED" } },
 		SAFE = { icon = "🦀", color = "safe", alt = { "SAFETY" } },
 		JUSTIFY = { icon = " ", color = "justify" },
 		FEAT = { icon = "", color = "feat", alt = { "IDEA" } },
 		IDK = { icon = "?", color = "idk", alt = { "HELP", "QUESTION" } },
+		HIGH = { icon = "H", color = "high", alt = { "H", "HI" } },
+		MEDIUM = { icon = "M", color = "medium", alt = { "M", "MED" } },
+		LOW = { icon = "L", color = "low", alt = { "L", "LO" } },
 	},
 	colors = {
-		check = {
-			-- "@lsp.type.string",
-			"#67eb34",
-		},
+		check = { "#67eb34" },
 		done = { "#42fcab" },
 		safe = { "#ffa75e" },
 		justify = { "#bc80ff" },
 		feat = { "#f5e618" },
 		idk = { "#fc4242" },
+		high = { "#fc4242" },
+		medium = { "#f7cf54" },
+		low = { "#996e29" },
 	},
 	-- highlight = {
 	-- 	pattern = [[.*<(KEYWORDS)\s*:?]], -- pattern or table of patterns, used for highlightng (vim regex)
